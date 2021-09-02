75392efd3482fd2a22056d2e0a17b18563cd3a6c
Yaroslava-Metelova <yaroslava.met@gmail.com>
2021-09-02 09:17:31 +0300

    boxwood commt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -15,7 +15,7 @@ fun main(args: Array<String>) {
 
     val log = Logger.getLogger(catFactsDataCollection?.javaClass?.name)
     log.info(catFactsDataCollection.toString())
-
+    //boxwood
 }
 
 fun requestDataFrom(baseUrl: String): Joke? {
